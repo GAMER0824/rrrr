a237aaabc1a92a7aef5331b7ec38e3b587a8997e
GAMER0824 <85885904+GAMER0824@users.noreply.github.com>
2021-07-02 09:27:26 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+var ground;
+var rock
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir: 0.05
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+   var rock_options = {
+    restitution: 0.99,
+    frictionAir: 0.05
+   }
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+
+  rock = Bodies.circle(250,10,30,rock_options);
+  World.add(world,rock);
+
+  stand = Bodies.rectangle(250,200,100,10,ground_options);
+  World.add(world,stand)
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,400,20);
+  ellipse(rock.position.x,rock.position.y,30,30);
+  rect(stand.position.x,stand.position.y,100,10)
+ 
+}
+
